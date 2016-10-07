b1e0c7179a17300ab966831697b5381587edf2b6
Siddharth Sharma <siddi.sharma@gmail.com>
2016-10-07 00:31:38 -0700

    Unbreakable, i hope.

--- app/models/messenger.rb
+++ app/models/messenger.rb
@@ -3,22 +3,42 @@ class Messenger
   WIT_ACCESS_TOKEN = "6NMGP4FDQ3COQMVYIV5NEHGQTLDUNRJF"
   def self.incoming(params)
     facebook_user_id = params["entry"][0]["messaging"][0]["sender"]["id"] # user id of sender
-    message = params["entry"][0]["messaging"][0]["message"] # message contents
+    
     messageCounter = 0
     greeting=0
     responded = "no" #flag to see if we've responded yet to the hello request
+    hasTask=false
+    hasTaskDay=false
 
-     wit_response = Messenger.wit(message["text"])
-     print(wit_response)
+     
      #check if the wit response was a "remind" intent
-     if !wit_response["entities"]["intent"].nil? && wit_response["entities"]["intent"][0]["value"] == "remind" 
-       #gets info about the reminder
-       intent = wit_response["entities"]["intent"][0]["value"]
-       task = wit_response["entities"]["agenda_entry"][0]["value"]
-       taskDay = wit_response["entities"]["datetime"][0]["value"]
-     elsif !wit_response["entities"]["Greetings"][0].nil?
-      greeting = 1
-     end
+     if !params["entry"][0]["messaging"].nil? && !params["entry"][0]["messaging"][0]["message"].nil? && !params["entry"][0]["messaging"][0]["message"]["text"].nil?
+      message = params["entry"][0]["messaging"][0]["message"]
+      wit_response = Messenger.wit(message["text"])
+      print(wit_response)
+       if !wit_response["entities"].nil? && !wit_response["entities"]["intent"].nil?
+         if !wit_response["entities"]["intent"].nil? && wit_response["entities"]["intent"][0]["value"] == "remind" 
+           #gets info about the reminder
+           intent = wit_response["entities"]["intent"][0]["value"]
+           
+           if !wit_response["entities"]["agenda_entry"].nil?
+            task = wit_response["entities"]["agenda_entry"][0]["value"]
+            hasTask = true
+           end
+
+         if !wit_response["entities"]["datetime"].nil?
+           taskDay = wit_response["entities"]["datetime"][0]["value"]
+           hasTaskDay = true
+          end
+        end
+      end
+
+      if !wit_response["entities"].nil? && !wit_response["entities"]["Greetings"].nil?
+          if !wit_response["entities"]["Greetings"][0].nil?
+          greeting = 1
+          end
+      end
+    end
 
     if greeting==1 && responded=="no"
       replyString = "Hey thanks for using Zeal, I'm a reminder bot."
@@ -31,7 +51,13 @@ class Messenger
 
     # check if incoming webhook contains a message
     # have a task with date, create reminder
-    if !message.nil? && !message["text"].nil? && !task.nil? && !taskDay.nil?
+
+    if !hasTask || !hasTaskDay
+      replyString = "Sorry we dont understand what you wrote, please try again"
+      Messenger.reply(replyString,messageCounter,facebook_user_id)
+      return true
+    end
+    if !message.nil? && !message["text"].nil? && !task.nil? && !taskDay.nil? && hasTask && hasTaskDay
       # call reply() method
       date_parsed = DateTime.parse(taskDay)
       reminder = Reminder.create(query: task, facebook_user_id: facebook_user_id, dueDate: date_parsed)
